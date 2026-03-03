0de17ff16364481872c0468af66c889c6411ffbc
Muhammad Bilal <muhamadbilalzulfiqar@gmail.com>
2026-03-03 21:18:39 +0500

    add log

--- app/Http/Controllers/DeviceTokenController.php
+++ app/Http/Controllers/DeviceTokenController.php
@@ -14,11 +14,16 @@ class DeviceTokenController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
+
+        Log::info('Device token request: ' . json_encode($request->all()));
+
         $data = $request->validate([
             'user_phone_e164' => 'required|exists:users,user_phone_e164',
             'token' => 'required|string|max:500',
             'platform' => 'nullable|string|in:android,ios',
         ]);
+
+        Log::info('Device token data: ' . json_encode($data));
 
         $platform = $data['platform'] ?? 'android';
 
@@ -32,6 +37,8 @@ class DeviceTokenController extends Controller
                 'last_used_at' => now(),
             ]
         );
+
+        Log::info('Device token registered for push notifications.');
 
         return response()->json([
             'status' => true,
